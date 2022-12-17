a71a509521cc41156385c38ea00911fd25cb9437
Abhinay <abhinayreddy0112@gmail.com>
2022-12-17 19:09:31 +0530

    Time: 740 ms (5.02%), Space: 14.4 MB (14.19%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,15 +1,16 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         arr=[]
+        nu2=len(nums2)
         for i in range(len(nums1)):
             k=nums2.index(nums1[i])
             maxi=nums1[i]
             print(arr)
-            for j in range(k,len(nums2)):
+            for j in range(k,nu2):
                 if maxi<nums2[j]:
                     arr.append(nums2[j])
                     break
-                elif j==len(nums2)-1:
+                elif j==nu2-1:
                     arr.append(-1)
             # if maxi==nums1[i]:
             #     arr.append(-1)
