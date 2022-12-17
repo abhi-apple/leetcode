5588c2df7ea41bb1f936e00a48e4d49a41c247ce
Abhinay <abhinayreddy0112@gmail.com>
2022-12-17 19:11:05 +0530

    Time: 53 ms (89.81%), Space: 14.3 MB (14.19%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,18 +1,38 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        arr=[]
-        nu2=len(nums2)
-        for i in range(len(nums1)):
-            k=nums2.index(nums1[i])
-            maxi=nums1[i]
-            print(arr)
-            for j in range(k,nu2):
-                if maxi<nums2[j]:
-                    arr.append(nums2[j])
+        nums2_indices = {}
+        for i, num in enumerate(nums2):
+            nums2_indices[num] = i
+
+        # Initialize an empty list to store the results
+        results = []
+
+        # For each element in nums1, find its index in nums2 and look for the next greater element
+        for num in nums1:
+            index = nums2_indices[num]
+            found = False
+            for j in range(index+1, len(nums2)):
+                if nums2[j] > num:
+                    results.append(nums2[j])
+                    found = True
                     break
-                elif j==nu2-1:
-                    arr.append(-1)
-            # if maxi==nums1[i]:
-            #     arr.append(-1)
+            if not found:
+                results.append(-1)
+
+        return results
+#         arr=[]
+#         nu2=len(nums2)
+#         for i in range(len(nums1)):
+#             k=nums2.index(nums1[i])
+#             maxi=nums1[i]
+#             print(arr)
+#             for j in range(k,nu2):
+#                 if maxi<nums2[j]:
+#                     arr.append(nums2[j])
+#                     break
+#                 elif j==nu2-1:
+#                     arr.append(-1)
+#             # if maxi==nums1[i]:
+#             #     arr.append(-1)
             
-        return arr
+#         return arr
