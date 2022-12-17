ca86f71fb253bf0b59797455dbfe065d10a0c0d0
Abhinay <abhinayreddy0112@gmail.com>
2022-12-18 04:42:12 +0530

    Time: 83 ms (68.06%), Space: 13.9 MB (65.14%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,17 +1,25 @@
 class Solution:
-    def commonChars(self, words: List[str]) -> List[str]:
-        com=list(words[0])
-        print(com)
-        if len(words)==1:
-            return list(words)
-        for i in words:
-            for j in list(com):
-                if j not in i:
-                    com.remove(j)
-                    print(j,i,"if")
-                else:
-                    i=i.replace(j, '', 1)
-                    print(j,i,"else")
-                    # i.replace(j, '')
+    def commonChars(self, A: List[str]) -> List[str]:
+        if len(A)<2 : return A
+        alist = set(A[0])     # make a set from first string
+        res = []
+        for one in alist:
+            n = min([a_word.count(one) for a_word in A])     # count min frequency of every letter in every word
+            if n:    # if n>0 , we append this letter n times
+                res += [one]*n
+        return res
+#         com=list(words[0])
+#         print(com)
+#         if len(words)==1:
+#             return list(words)
+#         for i in words:
+#             for j in list(com):
+#                 if j not in i:
+#                     com.remove(j)
+#                     print(j,i,"if")
+#                 else:
+#                     i=i.replace(j, '', 1)
+#                     print(j,i,"else")
+#                     # i.replace(j, '')
                     
-        return com
+#         return com
