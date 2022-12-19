8760e5e693d7243c1a484c650d9fd2d18aa505e5
Abhinay <abhinayreddy0112@gmail.com>
2022-12-19 11:02:38 +0530

    Time: 228 ms (70.80%), Space: 17.6 MB (72.95%) - LeetHub

--- 0384-shuffle-an-array/0384-shuffle-an-array.py
+++ 0384-shuffle-an-array/0384-shuffle-an-array.py
@@ -1,38 +1,21 @@
-import random
 class Solution:
+
     def __init__(self, nums: List[int]):
-        self.arr=nums
+        self.arr = nums
         self.real=nums[:]
+        # self.original = nums
+        # self.nums = nums
     def reset(self) -> List[int]:
         return self.real
+
     def shuffle(self) -> List[int]:
-        narr=self.arr
-        n=len(narr)
-        for i in range(n):
-            a=random.randint(0,n-1)
-            b=random.randint(0,n-1)
-            narr[a],narr[b]=narr[b],narr[a]
-        return narr
-
-# class Solution:
-
-#     def __init__(self, nums: List[int]):
-#         self.arr = nums[:]
-#         self.real=nums[:]
-#         # self.original = nums
-#         # self.nums = nums
-#     def reset(self) -> List[int]:
-#         print()
-#         return self.real
-
-#     def shuffle(self) -> List[int]:
-#         ans = self.arr[:]
-#         # ans.append(ans.pop(ans.index(ans[0])))
+        ans = self.arr
+        # ans.append(ans.pop(ans.index(ans[0])))
         
-#         for i in range(len(ans)):
-#             a=random.randint(0,len(ans)-1)
-#             b=random.randint(0,len(ans)-1)
-#             ans[a],ans[b]=ans[b],ans[a]
+        for i in range(len(ans)):
+            a=random.randint(0,len(ans)-1)
+            b=random.randint(0,len(ans)-1)
+            ans[a],ans[b]=ans[b],ans[a]
         # print(self.nums,"shu")
         # print(self.original,"ress")
         return ans
