02cfddde7cbe9505cf353d3c5664d6dd350e4c56
Abhinay <abhinayreddy0112@gmail.com>
2022-12-19 05:56:30 +0530

    Time: 2000 ms (44.65%), Space: 74.7 MB (91.37%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,46 +1,46 @@
-class LRUCache:
-    def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.values = OrderedDict()
-
-    def get(self, key: int) -> int:
-        if key not in self.values:
-            return -1
-        else:
-            self.values[key] = self.values.pop(key)
-            return self.values[key]
-
-    def put(self, key: int, value: int) -> None:
-        if key not in self.values:
-            if len(self.values) == self.capacity:
-                self.values.popitem(last=False)
-        else:
-            self.values.pop(key)
-        self.values[key] = value
-
 # class LRUCache:
-
 #     def __init__(self, capacity: int):
-#         self.size = capacity
-#         self.dic=OrderedDict()
+#         self.capacity = capacity
+#         self.values = OrderedDict()
 
 #     def get(self, key: int) -> int:
-#         if key in self.dic:
-#             print(self.dic)
-#             self.dic[key]=self.dic.pop(key)
-#             return self.dic[key]
-#         else:
+#         if key not in self.values:
 #             return -1
+#         else:
+#             self.values[key] = self.values.pop(key)
+#             return self.values[key]
 
 #     def put(self, key: int, value: int) -> None:
-#         if key not in self.dic:
-            
-#             if len(self.dic)==self.size:
-#                 self.dic.popitem(last=False)
-              
+#         if key not in self.values:
+#             if len(self.values) == self.capacity:
+#                 self.values.popitem(last=False)
 #         else:
-#             self.dic.pop(key)
-#         self.dic[key]=value
+#             self.values.pop(key)
+#         self.values[key] = value
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.size = capacity
+        self.dic=OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key in self.dic:
+
+            self.dic[key]=self.dic.pop(key)
+            return self.dic[key]
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key not in self.dic:
+            
+            if len(self.dic)==self.size:
+                self.dic.popitem(last=False)
+              
+        else:
+            self.dic.pop(key)
+        self.dic[key]=value
         
 
 
