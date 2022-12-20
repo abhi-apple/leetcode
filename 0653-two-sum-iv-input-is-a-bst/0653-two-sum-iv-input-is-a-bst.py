8a2690d7ff56293ebe842baf4a61b49a653d25ff
Abhinay <abhinayreddy0112@gmail.com>
2022-12-20 09:49:57 +0530

    Time: 119 ms (73.18%), Space: 20.2 MB (20.12%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -6,21 +6,39 @@
 #         self.right = right
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        def appendValuesToList(root, values):
-            if not root:
-                return
-
-            values.append(root.val)
-
-            appendValuesToList(root.left, values)
-            appendValuesToList(root.right, values)
-        values=[]
-        appendValuesToList(root,values)
-        seen=set()
-        for i in values:
-            if k-i in seen:
+        elem=[]
+        def inor(node):
+            if not node:
+                return 
+            inor(node.left)
+            elem.append(node.val)
+            inor(node.right)
+        inor(root)
+        left=0
+        rgt=len(elem)-1
+        while left<rgt:
+            if elem[left]+elem[rgt]==k:
                 return True
-            seen.add(i)
+            elif elem[left]+elem[rgt]<k:
+                left+=1
+            else:
+                rgt-=1
         return False
+#         def appendValuesToList(root, values):
+#             if not root:
+#                 return
+
+#             values.append(root.val)
+
+#             appendValuesToList(root.left, values)
+#             appendValuesToList(root.right, values)
+#         values=[]
+#         appendValuesToList(root,values)
+#         seen=set()
+#         for i in values:
+#             if k-i in seen:
+#                 return True
+#             seen.add(i)
+#         return False
 
         
