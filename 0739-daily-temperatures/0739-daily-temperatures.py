ec7e8dfaa612c4854315953678f8244f451f821d
Abhinay <abhinayreddy0112@gmail.com>
2022-12-26 05:51:41 +0530

    Time: 1770 ms (69.18%), Space: 28.4 MB (76.38%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -5,27 +5,14 @@ class Solution:
         result = [0] * len(temperatures)
         # Iterate through the temperatures
         for i, t in enumerate(temperatures):
-            # Pop the top element from the stack if it is smaller than the current temperature
+
+            print(i,t)
+            
             while stack and temperatures[stack[-1]] < t:
                 j = stack.pop()
-                # Calculate the waiting days and update the result array
                 result[j] = i - j
-            # Push the current index to the stack
             stack.append(i)
         return result
-#         res=[]
-    
-#         for i in range(len(temperatures)):
-#             ad=False
-#             for j in range(i,len(temperatures)):
-#                 if temperatures[j] >temperatures[i]:
-#                     ad=True
-#                     res.append(j-i)
-#                     break
-#             if ad==False:
-#                 res.append(0)
-#         return res
-                
             
             
             
