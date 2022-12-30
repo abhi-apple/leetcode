93f9e509b52e4a3a20deabf74cebffaac8c5881c
Abhinay <abhinayreddy0112@gmail.com>
2022-12-30 07:07:46 +0530

    Time: 41 ms (93.77%), Space: 15.4 MB (40.19%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -7,10 +7,3 @@ class Solution:
             
         s=sorted(otp,key=len,reverse=True)
         return "".join(s)
-#         output=[]
-#         for i in set(s):
-#             count=s.count(i)
-#             output.append(i*count)
-            
-#         output=sorted(output,key=len,reverse=True)
-#         return "".join(output)
