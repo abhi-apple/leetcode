d92a3dbbd3b7dd06ebc3726edc696a3f3880cc6d
Abhinay <abhinayreddy0112@gmail.com>
2023-01-01 14:50:35 +0530

    Time: 32 ms (85.55%), Space: 13.8 MB (74.33%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -5,20 +5,42 @@ class Solution:
         if len(pattern) != len(words):  # Check if the number of characters in the pattern and the number of words in the string are equal
             return False
 
-        mapping = {}  # Initialize an empty dictionary to store the mapping between characters and words
+        mapping = [-1] * 26  # Initialize an array of size 26 to store the mapping between characters and words
         for i in range(len(pattern)):
-            char = pattern[i]
+            char = ord(pattern[i]) - ord('a')  # Convert the character to an integer index in the range 0-25
             word = words[i]
 
-            if char in mapping:  # If the character is already in the mapping, check if it maps to the same word as before
+            if mapping[char] != -1:  # If the character is already in the mapping, check if it maps to the same word as before
                 if mapping[char] != word:
                     return False
             else:  # If the character is not in the mapping, check if the word is already mapped to a different character
-                if word in mapping.values():
-                    return False
-                mapping[char] = word  # Add the mapping to the dictionary
+                for j in range(26):
+                    if mapping[j] == word:
+                        return False
+                mapping[char] = word  # Add the mapping to the array
 
         return True  # If all checks pass, return True
+
+#     def wordPattern(self, pattern: str, s: str) -> bool:
+#         words = s.split()  # Split the string into a list of words
+
+#         if len(pattern) != len(words):  # Check if the number of characters in the pattern and the number of words in the string are equal
+#             return False
+
+#         mapping = {}  # Initialize an empty dictionary to store the mapping between characters and words
+#         for i in range(len(pattern)):
+#             char = pattern[i]
+#             word = words[i]
+
+#             if char in mapping:  # If the character is already in the mapping, check if it maps to the same word as before
+#                 if mapping[char] != word:
+#                     return False
+#             else:  # If the character is not in the mapping, check if the word is already mapped to a different character
+#                 if word in mapping.values():
+#                     return False
+#                 mapping[char] = word  # Add the mapping to the dictionary
+
+#         return True  # If all checks pass, return True
 
     # def wordPattern(self, pt: str, s: str) -> bool:
     #     words=s.split()
