42255c0bce6978c46242f09387252615a020e9fc
Abhinay <abhinayreddy0112@gmail.com>
2023-01-01 06:19:03 +0530

    Time: 32 ms (85.55%), Space: 13.9 MB (22.42%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,39 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()  # Split the string into a list of words
+
+        if len(pattern) != len(words):  # Check if the number of characters in the pattern and the number of words in the string are equal
+            return False
+
+        mapping = {}  # Initialize an empty dictionary to store the mapping between characters and words
+        for i in range(len(pattern)):
+            char = pattern[i]
+            word = words[i]
+
+            if char in mapping:  # If the character is already in the mapping, check if it maps to the same word as before
+                if mapping[char] != word:
+                    return False
+            else:  # If the character is not in the mapping, check if the word is already mapped to a different character
+                if word in mapping.values():
+                    return False
+                mapping[char] = word  # Add the mapping to the dictionary
+
+        return True  # If all checks pass, return True
+
+    # def wordPattern(self, pt: str, s: str) -> bool:
+    #     words=s.split()
+    #     print(words)
+    #     if len(words)!=len(s):
+    #         return False
+    #     res={}
+    #     for i in range(len(pt)):
+    #         char=pt[i]
+    #         wrd=words[i]
+    #         if char in res:
+    #             if res[char]!=wrd:
+    #                 return False
+    #         else:
+    #             if wrd in res.values():
+    #                 return False
+    #             res[char]=wrd
+    #     return True
