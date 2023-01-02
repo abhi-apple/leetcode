cecde3dcc1db286222eba8490545849e5bbc8a6d
Abhinay <abhinayreddy0112@gmail.com>
2023-01-02 06:15:00 +0530

    Time: 42 ms (65.65%), Space: 13.9 MB (56.30%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -1,13 +1,14 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        if len(word)==1:
+        # Check if all characters are capital letters
+        if word.isupper():
             return True
-        for i in range(1,len(word)):
-            if ord(word[0])>=97 and ord(word[i])<97:
-                return False 
-            else:
-                if ord(word[1])<97 and ord(word[i])>=97:
-                    return False 
-                elif ord(word[i])<97 and ord(word[1])>=97:
-                    return False 
-        return True
+        # Check if all characters are not capital letters
+        elif word.islower():
+            return True
+        # Check if only the first character is a capital letter
+        elif len(word) > 1 and word[0].isupper() and word[1:].islower():
+            return True
+        # Otherwise, capital usage is not correct
+        else:
+            return False
