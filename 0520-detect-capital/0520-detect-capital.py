06528e7a46ce6ec2b36978c1484d3a4410286f3b
Abhinay <abhinayreddy0112@gmail.com>
2023-01-02 06:13:29 +0530

    Time: 43 ms (63.78%), Space: 13.8 MB (56.30%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -3,15 +3,11 @@ class Solution:
         if len(word)==1:
             return True
         for i in range(1,len(word)):
-            print(ord(word[i]))
             if ord(word[0])>=97 and ord(word[i])<97:
-                print("h1")
                 return False 
             else:
                 if ord(word[1])<97 and ord(word[i])>=97:
-                    print("h2")
                     return False 
                 elif ord(word[i])<97 and ord(word[1])>=97:
-                    print("h3")
                     return False 
         return True
