a1a923fa59cbbf86a6ca792edd4c99fd19013ed1
Abhinay <abhinayreddy0112@gmail.com>
2023-01-06 07:01:05 +0530

    Time: 58 ms (52.68%), Space: 14 MB (35.07%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -12,19 +12,4 @@ class Solution:
                     dict[word]+=1
                 else:
                     dict[word]=1
-		#Don't need a counter if you use key function to choose the key with max. count!
         return max(dict, key=dict.get)
-        # def remove_special_characters(s):
-        #     return re.sub(r'[^\w\s]', '', s)
-        # para=para.replace('.','')
-        # res=para.replace(",",'')
-        # para=remove_special_characters(para)
-        # para=list(para.split(" "))
-        # para=list(map(str.lower,para))
-        # dic={}
-        # for i in para:
-        #     if i in dic and i not in  ban:
-        #         dic[i]+=1
-        #     elif i not in ban:
-        #         dic[i]=1
-        # return max(dic, key= lambda x: dic[x])
