5eddb26489e4bd05322c9ffb100064ad9fa29862
Abhinay <abhinayreddy0112@gmail.com>
2023-01-13 09:42:47 +0530

    Time: 58 ms (89.61%), Space: 14 MB (63.78%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,14 +1,27 @@
 class Solution:
-    def combinationSum(self, cd: List[int], target: int) -> List[List[int]]:
-        def dfs(cd,target,start,path,res):
-            if target<0:
-                return 
-            if target==0:
-                res.append(path)
-                return
-            for i in range(start,len(cd)):
-                dfs(cd,target-cd[i],i,path+[cd[i]],res)
+    def combinationSum(self, cd: List[int], tar: int) -> List[List[int]]:
         res=[]
-        cd.sort()
-        dfs(cd,target,0,[],res)
+        def dfs(i,cur,tot):
+            if tot==tar:
+                res.append(cur.copy())
+                return
+            if i >=len(cd) or tot>tar:
+                return
+            cur.append(cd[i])
+            dfs(i,cur,tot+cd[i])
+            cur.pop()
+            dfs(i+1,cur,tot)
+        dfs(0,[],0)
         return res
+        # def dfs(cd,target,start,path,res):
+        #     if target<0:
+        #         return 
+        #     if target==0:
+        #         res.append(path)
+        #         return
+        #     for i in range(start,len(cd)):
+        #         dfs(cd,target-cd[i],i,path+[cd[i]],res)
+        # res=[]
+        # cd.sort()
+        # dfs(cd,target,0,[],res)
+        # return res
