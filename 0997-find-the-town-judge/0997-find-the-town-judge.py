45a749040f35871f76e7c4dfdec60b828b737dcf
Abhinay <abhinayreddy0112@gmail.com>
2023-01-23 06:05:52 +0530

    Time: 944 ms (40.51%), Space: 18.8 MB (99.48%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,18 +1,18 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        ans=[]
-        fs=[]
+        ans={}
+        f=[]
         if n==1:
             return 1
-        if len(trust)==1:
-            return trust[0][1]
         for i in range(len(trust)):
             a,b=trust[i]
-            fs.append(a)
-            ans.append(b)
-        
+            f.append(a)
+            if b not in ans:
+                ans[b]=1
+            else:
+                ans[b]+=1
+                
         for i in ans:
-            if i not in fs and ans.count(i)==n-1:
+            if ans[i]==n-1 and i not in f:
                 return i
         return -1
-            
