bd2c5d1a993e1b4082bb805a8ee933986c614810
Abhinay <abhinayreddy0112@gmail.com>
2023-01-25 07:05:07 +0530

    Time: 35 ms (94.49%), Space: 14.2 MB (48.48%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
@@ -10,6 +10,5 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        nex=node.next
-        node.val=nex.val
-        node.next=nex.next
+        node.val=node.next.val
+        node.next=node.next.next
