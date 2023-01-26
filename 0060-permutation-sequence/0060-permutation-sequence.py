d37b26e2a8b3488c40022ac9f827957dc1f1990d
Abhinay <abhinayreddy0112@gmail.com>
2023-01-26 18:00:04 +0530

    Time: 39 ms (54.67%), Space: 14 MB (31.17%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,16 +1,18 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        res = []
-        nums = list(range(1, n+1))
-        factorial = [1] * (n + 1)
-        for i in range(1, n+1):
-            factorial[i] = factorial[i-1] * i
-        k -= 1
-        for i in range(n, 0, -1):
-            first_num_index = k // factorial[i-1]
-            res.append(nums[first_num_index])
-            nums.pop(first_num_index)
-            k -= first_num_index * factorial[i-1]
-        return "".join(map(str, res))
-
-        
+            fac=1
+            nums=[]
+            for i in range(1,n):
+                fac=fac*i
+                nums.append(i)
+            nums.append(n)
+            ans=''
+            k=k-1
+            while True:
+                ans=ans+str(nums[int(k//fac)])
+                nums.remove(nums[int(k//fac)])
+                if len(nums)==0:
+                    break;
+                k=k%fac
+                fac=fac/len(nums)
+            return ans
