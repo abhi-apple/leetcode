0f6e6379d1c5aeffeb97f031fea71e27510294e0
Abhinay <abhinayreddy0112@gmail.com>
2023-02-07 08:06:30 +0530

    Time: 1025 ms (55.33%), Space: 20.2 MB (51.27%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,20 +1,19 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        max_length, win_start = 0, 0
-        fruit_freq = {}
-        
-        for win_end in range(len(fruits)):
-            fruit_left = fruits[win_end]
-            if fruit_left not in fruit_freq:
-                fruit_freq[fruit_left] = 0
-            fruit_freq[fruit_left] += 1
-            
-            while len(fruit_freq) > 2:
-                fruit_right = fruits[win_start]
-                fruit_freq[fruit_right] -= 1
-                if fruit_freq[fruit_right] == 0:
-                    del fruit_freq[fruit_right]
-                win_start += 1
-            
-            max_length = max(max_length, win_end - win_start + 1)
-        return max_length
+        left, right, max_fruits = 0, 0, 0
+        fruit_count = {}
+
+        while right < len(fruits):
+            if fruits[right] in fruit_count:
+                fruit_count[fruits[right]] += 1
+            else:
+                fruit_count[fruits[right]] = 1
+                while len(fruit_count) > 2:
+                    fruit_count[fruits[left]] -= 1
+                    if fruit_count[fruits[left]] == 0:
+                        del fruit_count[fruits[left]]
+                    left += 1
+            right += 1
+            max_fruits = max(max_fruits, right - left)
+
+        return max_fruits
