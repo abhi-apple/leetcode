aa8e3b2c928e18c2e22007d2b66895c83239d838
Abhinay <abhinayreddy0112@gmail.com>
2023-02-14 05:50:54 +0530

    Time: 38 ms (53.43%), Space: 13.8 MB (97.46%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,16 +1,5 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        res=""
-        car=0
-        if len(a)<len(b):
-            a="0"*(len(b)-len(a))+a
-        elif len(b)<len(a):
-            b="0"*(len(a)-len(b))+b
-        for i in range(len(a)-1,-1,-1):
-            sums=int(a[i])+int(b[i])+car
-            res+=str(sums%2)
-            car=sums//2
-        if car==1:
-            res+="1"
-        return res[::-1]
-        
+        result = bin(int(a, 2) + int(b, 2))
+        result = format(int(result, 2), 'b')
+        return str(result)
