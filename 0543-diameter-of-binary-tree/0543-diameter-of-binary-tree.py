cbe42ff048475460b0d4d0ff23a3e236d9855e19
Abhinay <abhinayreddy0112@gmail.com>
2023-03-01 11:19:01 +0530

    Time: 48 ms (60.75%), Space: 16.4 MB (33.45%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -6,14 +6,27 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        def heig(root,diam):
-            if not root:
-                return 0
-            lh=heig(root.left,diam)
-            rh=heig(root.right,diam)
-            diam[0]=max(diam[0],lh+rh)
-            return max(lh,rh)+1
-        diam=[0]
-        heig(root,diam)
-        return diam[0]
-    
+        def diameter(node):
+            if not node:
+                return (0, 0)
+            left_height, left_diameter = diameter(node.left)
+            right_height, right_diameter = diameter(node.right)
+            current_diameter = left_height + right_height
+            max_diameter = max(left_diameter, right_diameter, current_diameter)
+            return (max(left_height, right_height) + 1, max_diameter)
+
+        return diameter(root)[1]
+
+# class Solution:
+#     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+#         def hei(node):
+#             if not node:
+#                 return 0
+#             return max(hei(node.left),hei(node.right))+1
+#         if not root:
+#             return 0
+#         op1=self.diameterOfBinaryTree(root.left)
+#         op2=self.diameterOfBinaryTree(root.right)
+#         op3=hei(root.left)+hei(root.right)+1
+#         return max(op1,op2,op3)
+        
