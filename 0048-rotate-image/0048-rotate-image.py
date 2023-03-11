6cb04ad984f75cd33d1a278093adf05cf7e03745
Abhinay <abhinayreddy0112@gmail.com>
2023-03-11 10:35:07 +0530

    Time: 41 ms (35.89%), Space: 13.9 MB (62.79%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,13 +1,28 @@
 
+# class Solution:
+#     def rotate(self, mat: List[List[int]]) -> None:
+#         """
+#         Do not return anything, modify matrix in-place instead.
+#         """
+        
+#         for i in range(len(mat)):
+#             for j in range(len(mat[0])):
+#                 mat[i][j],mat[j][i]=mat[j][i],mat[i][j]
+#         for i in range(len(mat)):
+#             mat[i].reverse()
+        
 class Solution:
     def rotate(self, mat: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        for i in range(len(mat)):
-            for j in range(i+1,len(mat[0])):
-                mat[i][j],mat[j][i]=mat[j][i],mat[i][j]
         
+        # Reverse the rows of the matrix
+        mat.reverse()
+        print(mat)
+        # Swap the elements of the matrix
         for i in range(len(mat)):
-            mat[i].reverse()
+            for j in range(i):
+                mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+
             
