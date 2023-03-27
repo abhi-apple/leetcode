97a767b5636ad833928b0433541c9d4363b63ed1
Abhinay <abhinayreddy0112@gmail.com>
2023-03-27 15:45:40 +0530

    Time: 37 ms (76.67%), Space: 14.1 MB (89.09%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,12 +1,31 @@
+# class Solution:
+#     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+#         ans=set()
+#         def subs(i,ls):
+#             if i==len(nums):
+#                 ls.sort()
+#                 ans.add(tuple(ls))
+#                 return 
+#             ls.append(nums[i])
+#             subs(i+1,ls)
+#             ls.pop()
+#             subs(i+1,ls)
+#         subs(0,[])
+#         return ans
+    
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        fin=[]
-        for i in range(2**(len(nums))):
-            ar=[]
-            for j in range(len(nums)):
-                if i & (1<<j):
-                    ar.append(nums[j])
-            ar.sort()
-            if ar not in fin:
-                fin.append(ar)
-        return fin
+        ans = set()
+        
+        def backtrack(start, path):
+            ans.add(tuple(path))
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i-1]:
+                    continue
+                path.append(nums[i])
+                backtrack(i+1, path)
+                path.pop()
+        
+        nums.sort()
+        backtrack(0, [])
+        return [list(x) for x in ans]
