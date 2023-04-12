e8f4df0fe41442729eb6abdddd3f1a6da09e2de8
Abhinay <abhinayreddy0112@gmail.com>
2023-04-12 07:10:04 +0530

    Time: 677 ms (37.89%), Space: 30.1 MB (73.74%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -2,19 +2,34 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res=[]
         pat=[]
-        def ispa(s, st, ed):
-            return s[st:ed+1] == s[st:ed+1][::-1]
-
-        def fu(ind,s,pat,res):
+        def ispa(st,ed):
+            return s[st:ed+1]==s[st:ed+1][::-1]
+        def rec(ind):
             if ind==len(s):
                 res.append(pat.copy())
-                return
             for i in range(ind,len(s)):
-                if ispa(s,ind,i):
+                if ispa(ind,i):
                     pat.append(s[ind:i+1])
-                    fu(i+1,s,pat,res)
+                    rec(i+1)
                     pat.pop()
+                    
+        rec(0)
+        return res
+#         res=[]
+#         pat=[]
+#         def ispa(s, st, ed):
+#             return s[st:ed+1] == s[st:ed+1][::-1]
+
+#         def fu(ind,s,pat,res):
+#             if ind==len(s):
+#                 res.append(pat.copy())
+#                 return
+#             for i in range(ind,len(s)):
+#                 if ispa(s,ind,i):
+#                     pat.append(s[ind:i+1])
+#                     fu(i+1,s,pat,res)
+#                     pat.pop()
         
                 
-        fu(0,s,pat,res)
-        return res
+#         fu(0,s,pat,res)
+#         return res
