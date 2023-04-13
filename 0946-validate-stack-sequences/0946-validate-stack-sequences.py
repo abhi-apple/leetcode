243bca1d955a334cf3fcb6d2699ac16f6ad90f02
Abhinay <abhinayreddy0112@gmail.com>
2023-04-13 05:46:44 +0530

    Time: 68 ms (87.13%), Space: 14.1 MB (34.41%) - LeetHub

---
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validateStackSequences(self, pu: List[int], po: List[int]) -> bool:
+        st=[]
+        j=0  # Move j outside of the for loop to prevent resetting it on each iteration
+        for i in pu:
+            st.append(i)
+            while st and  po[j]==st[-1]:
+                st.pop()
+                j+=1
+                if j==len(po):  # Fix the comparison to len(po) instead of len(po)-1
+                    return True
+        return not st and j==len(po)  # Add a check to ensure stack is empty and all elements of po have been processed
