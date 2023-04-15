4faa01383b3753d688301ca1e872b8621597ad5b
Abhinay <abhinayreddy0112@gmail.com>
2023-04-15 12:11:42 +0530

    Time: 971 ms (5.08%), Space: 13.9 MB (60.39%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -12,40 +12,38 @@ class Solution:
                 if board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] == str(c):
                     return False
             return True
-
-        def solveSudoku(board):
-            for i in range(len(board)):
-                for j in range(len(board[0])):
-                    if board[i][j] == '.':
+        def sud(bor):
+            for i in range(len(bor)):
+                for j in range(len(bor[0])):
+                    if bor[i][j]=='.':
                         for c in range(1,10):
-                            if isValid(board, i, j, c):
-                                board[i][j] = str(c)
-                                if solveSudoku(board):
+                            if isValid(bor,i,j,c):
+                                bor[i][j]=str(c)
+                                if sud(bor):
                                     return True
                                 else:
-                                    board[i][j] = '.'
+                                    bor[i][j]='.'
                         return False
             return True
-        solveSudoku(bo)
+                                
+        
+        sud(bo)
 
-        # def solve(bo):
-        #     for i in range(len(bo)):
-        #         for j in range(len(bo[0])):
-        #             if bo[i][j]=='.':
-        #                 for c in range(10):
-        #                     if valid(bo,i,j,c):
-        #                         bo[i][j]=str(c)
-        #                         if solve(bo)==True:
-        #                             return True
-        #                         else:
-        #                             bo[i][j]='.'
-        #                 return False
-        #     return True
-        # def valid(bo,row,col,c):
-        #     for i in range(10):
-        #         if bo[i][col]==str(c) or bo[row][i]==str(c) or bo[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] == str(c):
-        #             return False
-        #     return True
+#         def solveSudoku(board):
+#             for i in range(len(board)):
+#                 for j in range(len(board[0])):
+#                     if board[i][j] == '.':
+#                         for c in range(1,10):
+#                             if isValid(board, i, j, c):
+#                                 board[i][j] = str(c)
+#                                 if solveSudoku(board):
+#                                     return True
+#                                 else:
+#                                     board[i][j] = '.'
+#                         return False
+#             return True
+#         solveSudoku(bo)
+
         
         
         
