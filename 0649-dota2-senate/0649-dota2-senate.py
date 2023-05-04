cd94ff389b8afb52255e6bf2519fb893933d65de
Abhinay <abhinayreddy0112@gmail.com>
2023-05-04 23:42:11 +0530

    Time: 49 ms (91.13%), Space: 16.8 MB (5.50%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -1,24 +1,20 @@
 class Solution:
-    def predictPartyVictory(self, senate: str) -> str:
-        n = len(senate)
-        s, banned = set(), [False] * n
-        ban_d = ban_r = 0
-        while len(s) != 1:
-            s = set()
-            for i, p in enumerate(senate):
-                if banned[i]: continue
-                if p == 'R':
-                    if ban_r > 0:           # current R being banned
-                        ban_r -= 1
-                        banned[i] = True
-                    else:                   # if current R is valid, it will ban D
-                        ban_d += 1
-                        s.add('R')
-                else:        
-                    if ban_d > 0:           # current D being banned
-                        ban_d -= 1
-                        banned[i] = True
-                    else:                   # if current D is valid, it will ban R
-                        ban_r += 1
-                        s.add('D')
-        return 'Radiant' if s.pop() == 'R' else 'Dire'
+    def predictPartyVictory(self, sen: str) -> str:
+        qr = deque()
+        qd=deque()
+        n=len(sen)
+        for i in range(n):
+            if sen[i]=='R':
+                qr.append(i)
+            else:
+                qd.append(i)
+        while qr and qd:
+            rid=qr.popleft()
+            did=qd.popleft()
+            if rid<did:
+                qr.append(rid+n)
+            else:
+                qd.append(did+n)
+        if qr:
+            return "Radiant"
+        return "Dire"
