141ac355e46fd6b50d3fd321c758fd6d678b13bb
Abhinay <abhinayreddy0112@gmail.com>
2023-05-06 12:25:39 +0530

    Time: 63 ms (72.16%), Space: 16.4 MB (5.05%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,20 +1,20 @@
 class Solution:
     def combinationSum2(self, cd: List[int], tar: int) -> List[List[int]]:
-        
-        def dfs(i,cd,t,ls,cur):
-            if tar==t:
-                ls.append(cur.copy())
-                return
-            for k in range(i,len(cd)):
-                if k>i and cd[k]==cd[k-1]:
-                    continue
-                if t+cd[k]>tar:
-                    break
-                cur.append(cd[k])
-                dfs(k+1,cd,t+cd[k],ls,cur)
-                cur.pop()
-        ls=[]
         cd.sort()
-        dfs(0,cd,0,ls,[])
-        return ls
+        res = set()
         
+        def dfs(i, ar, t):
+            if t == tar:
+                res.add(tuple(ar))
+                return
+            for j in range(i,len(cd)):
+                if j>i and cd[j]==cd[j-1]:
+                    continue
+                if t + cd[j] > tar:
+                    break
+                ar.append(cd[j])
+                dfs(j+1, ar, t+cd[j])
+                ar.pop()
+        
+        dfs(0, [], 0)
+        return res
