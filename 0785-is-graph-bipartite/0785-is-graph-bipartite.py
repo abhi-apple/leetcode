f8fcef6a4c019bb6d7b91c40e12c4b7253758194
Abhinay <abhinayreddy0112@gmail.com>
2023-05-19 07:37:33 +0530

    Time: 191 ms (31.38%), Space: 17 MB (12.98%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,60 +1,29 @@
-# class Solution:
-#     def isBipartite(self, graph: List[List[int]]) -> bool:
-#         col=[-1 for i in range(len(graph))]
-#         adj = {}
-#         for vertex in range(len(graph)):
-#             neighbors = []
-#             for neighbor in graph[vertex]:
-#                 neighbors.append(neighbor)
-#             adj[vertex] = neighbors
-        
-#         def bfs(ind):
-#             que=deque()
-#             que.append(ind)
-#             col[ind]=1
-#             while que:
-#                 val=que.popleft()
-#                 for i in adj[val]:
-#                     if col[i]==-1:
-#                         col[i]=1-col[val]
-#                         que.append(i)
-#                     elif col[i]==col[val]:
-#                         return False
-            
-            
-#         for i in range(len(col)):
-#             if col[i]==-1:
-#                 if not bfs(i):
-#                     return False
-#         return True
-    
-from typing import List
-from collections import deque
-
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         n = len(graph)
-        colors = [-1] * n
-
-        def bfs(start):
-            queue = deque()
-            queue.append(start)
-            colors[start] = 0
-
-            while queue:
-                node = queue.popleft()
-                for neighbor in graph[node]:
-                    if colors[neighbor] == -1:
-                        colors[neighbor] = 1 - colors[node]
-                        queue.append(neighbor)
-                    elif colors[neighbor] == colors[node]:
+        col=[-1]*n
+        
+        def bfs(ind):
+            que=deque()
+            que.append(ind)
+            col[ind]=1
+            while que:
+                print(col)
+                val=que.popleft()
+            
+                for i in graph[val]:
+                    if col[i]==-1:
+                        col[i]= not col[val]
+                        que.append(i)
+                    elif col[i]==col[val]:
+                        print(i,val)
                         return False
-
             return True
-
-        for i in range(n):
-            if colors[i] == -1:
+            
+        for i in range(len(col)):
+            if col[i]==-1:
                 if not bfs(i):
+                    print(i)
                     return False
-
         return True
+    
