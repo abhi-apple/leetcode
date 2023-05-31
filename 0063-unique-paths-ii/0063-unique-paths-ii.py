52f02a8509ab00e70a96a796128fa2803d05e093
Abhinay <abhinayreddy0112@gmail.com>
2023-05-31 19:19:01 +0530

    Time: 68 ms (6.04%), Space: 16.6 MB (13.11%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,21 +1,25 @@
+from typing import List
+
 class Solution:
-    def uniquePathsWithObstacles(self, obg: List[List[int]]) -> int:
-        dp=[[0]*len(obg[0])]*len(obg)
-        dp[0][0]=1
-        if obg[0][0]==1 or obg[len(obg)-1][len(obg[0])-1]==1:
-            return 0
-        for i in range(len(obg)):
-            for j in range(len(obg[0])):
-                if i==0 and j==0:
-                    continue
-                elif obg[i][j]!=1:
-                    up=0
-                    dw=0
-                    if i>0 and obg[i-1][j]!=1:
-                        up=dp[i-1][j]
-                    if j>0 and obg[i][j-1]!=1:
-                        dw=dp[i][j-1] 
-                    dp[i][j]=up+dw
-        return dp[len(obg)-1][len(obg[0])-1]
-                    
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        dp = [[-1] * n for _ in range(m)]
         
+        def rec(i, j):
+            if i == 0 and j == 0:
+                return 1
+            
+            if i < 0 or j < 0 or obstacleGrid[i][j] == 1:
+                return 0
+            
+            if dp[i][j] != -1:
+                return dp[i][j]
+            
+            dp[i][j] = rec(i-1, j) + rec(i, j-1)
+            return dp[i][j]
+        
+        if obstacleGrid[0][0] == 1 or obstacleGrid[m-1][n-1] == 1:
+            return 0
+        
+        return rec(m-1, n-1)
