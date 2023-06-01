400f3501d842fc689963ac032fa5bce3723a31ee
Abhinay <abhinayreddy0112@gmail.com>
2023-06-01 23:29:13 +0530

    Time: 2775 ms (5.01%), Space: 387.3 MB (5.19%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,25 +1,25 @@
 class Solution:
     def coinChange(self, coins: List[int], am: int) -> int:
+        dic={}
         n=len(coins)
-        dp=[[-1]*(am+1) for i in range(n)]
-        def f(ind,w):
-            if ind==0:
-                if w%coins[0]==0:
-                    return w//coins[0]
+        def rec(i,m):
+            if i==n-1:
+                if m%coins[-1]==0:
+                    return m//coins[-1]
                 else:
-                    return 100000
-            if dp[ind][w]!=-1:
-                return dp[ind][w]
-            nt=f(ind-1,w)
-            tk=100000
-            if coins[ind]<=w:
-                tk=1+f(ind,w-coins[ind])
-            dp[ind][w]=min(tk,nt)
-            return min(tk,nt)
+                    return float('inf')
+            if (i,m) in dic:
+                return dic[(i,m)]
             
-        ans=f(n-1,am)
-        if ans>=100000:
+            nt=rec(i+1,m)
+            tk=float('inf')
+            if coins[i]<=m:
+                tk=1+rec(i,m-coins[i])
+            dic[(i,m)]=min(nt,tk)
+            return min(nt,tk)
+            
+        ans=rec(0,am)
+        if ans==float('inf'):
             return -1
-        
         return ans
             
