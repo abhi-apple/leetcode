19de12cb04366af766a9c9273975bca1c73b3aca
Abhinay <abhinayreddy0112@gmail.com>
2023-06-02 00:00:59 +0530

    Time: 348 ms (53.01%), Space: 34.2 MB (48.26%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,26 +1,18 @@
 class Solution:
-    def findTargetSumWays(self, nums: List[int], tar: int) -> int:
-        tsum=sum(nums)
-        n=len(nums)
-        if tsum-tar<0:
-            return 0
-        if (tsum-tar)%2==1:
-            return 0
-        s2=(tsum-tar)//2
-        dp = [[-1 for i in range(s2+1)] for j in range(n)]
-        def fres(ind,ta):
-            if ind==0:
-                if ta==0 and nums[0]==0:
-                    return 2
-                if ta==0 or ta==nums[0]:
+    def findTargetSumWays(self, nums: List[int], t: int) -> int:
+        dp = {}
+
+        def rec(i, tar):
+            if i == -1:
+                if tar == 0:
                     return 1
-                return 0
-            if dp[ind][ta]!=-1:
-                return dp[ind][ta]
-            nt=fres(ind-1,ta)
-            tk=0
-            if nums[ind]<=ta:
-                tk=fres(ind-1,ta-nums[ind])
-            dp[ind][ta]=nt+tk
-            return nt+tk
-        return fres(n-1,s2)
+                else:
+                    return 0
+            if (i, tar) in dp:
+                return dp[(i, tar)]
+            nt = rec(i - 1, tar - nums[i])
+            tk = rec(i - 1, tar + nums[i])
+            dp[(i, tar)] = nt + tk
+            return dp[(i, tar)]
+
+        return rec(len(nums) - 1, t)
