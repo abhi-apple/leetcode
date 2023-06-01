0234b2ab29b9c76a8c739704f85acf1d87621535
Abhinay <abhinayreddy0112@gmail.com>
2023-06-01 10:45:18 +0530

    Time: 919 ms (38.10%), Space: 17.2 MB (45.80%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,23 +1,6 @@
 class Solution:
     def twoSum(self, main: List[int], tar: int) -> List[int]:
-        res=main.copy()
-        res.sort()
-        i=0
-        j=len(res)-1
-        ans=[]
-        while i<j:
-            if res[i]+res[j]==tar:
-                n1=res[i]
-                n2=res[j]
-                break
-            elif res[i]+res[j]>tar:
-                j-=1
-            else:
-                i+=1
-        for k in range(len(res)):
-            if main[k]==n1 or main[k]==n2:
-                ans.append(k)
-        return ans
-            
-        
-        
+        for i in range(len(main)):
+            if tar-main[i] in main and main.index(tar-main[i])!=i :
+                return [i,main.index(tar-main[i])]
+        return
