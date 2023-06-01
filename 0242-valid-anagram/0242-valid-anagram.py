3b08b917815e6bfc0896075eb79b2e56bc853037
Abhinay <abhinayreddy0112@gmail.com>
2023-06-01 10:39:49 +0530

    Time: 75 ms (19.47%), Space: 17.1 MB (19.31%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s=''.join(sorted(s))
+        t=''.join(sorted(t))
+        if s==t:
+            return True
+        return False
