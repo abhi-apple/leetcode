f48e187b54f30bd2cb12882c5e15d3c86d3e5aff
Abhinay <abhinayreddy0112@gmail.com>
2023-06-02 22:37:20 +0530

    Time: 2469 ms (5.00%), Space: 359.7 MB (5.01%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,16 +1,17 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-        r=s[::-1]
-        n=len(s)
-        dp=[[-1 for i in range(n+1)] for j in range(n+1)]
-        for i in range(n+1):
-            dp[i][0]=0
-        for j in range(n+1):
-            dp[0][j]=0
-        for i in range(1,n+1):
-            for j in range(1,n+1):
-                if s[i-1]==r[j-1]:
-                    dp[i][j]=1+dp[i-1][j-1]
-                else:
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-        return n-dp[n][n]
+        t=s[::-1]
+        if t==s:
+            return 0
+        dp={}
+        def rec(i,j):
+            if i==-1 or j==-1:
+                return 0
+            if (i,j) in dp:
+                return dp[(i,j)]
+            if s[i]==t[j]:
+                dp[(i,j)]=rec(i-1,j-1)+1
+            else:
+                dp[(i,j)]=max(rec(i-1,j),rec(i,j-1))
+            return dp[(i,j)]
+        return len(s)-rec(len(s)-1,len(s)-1)
