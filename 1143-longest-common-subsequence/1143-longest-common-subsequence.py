13e5a8d329c8f9e02314e554c4c55d27f5bc2e00
Abhinay <abhinayreddy0112@gmail.com>
2023-06-02 20:25:17 +0530

    Time: 2756 ms (5.00%), Space: 310.3 MB (6.84%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,25 +1,18 @@
 class Solution:
     def longestCommonSubsequence(self, s1: str, s2: str) -> int:
-        # def f(i1,i2):
-        #     if i1<0 or i2<0:
-        #         return 0
-        #     if dp[i1][i2]!=-1:
-        #         return dp[i1][i2]
-        #     if s1[i1]==s2[i2]:
-        #         dp[i1][i2]=1+f(i1-1,i2-1)
-        #         return 1+f(i1-1,i2-1)
-        #     else:
-        #         dp[i1][i2]=max(f(i1-1,i2),f(i1,i2-1))
-        #         return max(f(i1-1,i2),f(i1,i2-1))
-        dp=[[-1 for i in range(len(s2)+1)] for j in range(len(s1)+1)]
-        for i in range(len(s1)+1):
-            dp[i][0]=0
-        for j in range(len(s2)+1):
-            dp[0][j]=0
-        for i in range(1,len(s1)+1):
-            for j in range(1,len(s2)+1):
-                if s1[i-1]==s2[j-1]:
-                    dp[i][j]=1+dp[i-1][j-1]
-                else:
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-        return dp[len(s1)][len(s2)]
+        dp={}
+        def rec(i,j):
+            if i==-1 or j==-1:
+                return 0
+            if (i,j) in dp:
+                return dp[(i,j)]
+            if s1[i]==s2[j]:
+                dp[(i,j)]=1+rec(i-1,j-1)
+                return dp[(i,j)]
+            else:
+                tk=rec(i-1,j)
+                bt=rec(i,j-1)
+                nt=rec(i-1,j-1)
+                dp[(i,j)]=max(tk,bt,nt)
+                return dp[(i,j)]
+        return rec(len(s1)-1,len(s2)-1)
