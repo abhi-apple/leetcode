6249f2fe40c26de30f30f6a4d0435bbcfffd7182
Abhinay <abhinayreddy0112@gmail.com>
2023-06-03 01:22:53 +0530

    Time: 102 ms (86.62%), Space: 19.1 MB (48.97%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,20 +1,24 @@
 class Solution:
     def minDistance(self, w1: str, w2: str) -> int:
-        dp=[[-1 for i in range(len(w2))] for j in range(len(w1))]
-        def f(i,j):
-            if i<0:
+        dp = {}
+
+        def rec(i, j):
+            if i == -1 :
                 return j+1
-            if j<0:
-                return i+1
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            if w1[i]==w2[j]:
-                dp[i][j]=f(i-1,j-1)
-                return f(i-1,j-1)
-            else:
-                dp[i][j]=1+min(f(i,j-1),f(i-1,j),f(i-1,j-1))
-                return 1+min(f(i,j-1),f(i-1,j),f(i-1,j-1))
-            
-            
-        return f(len(w1)-1,len(w2)-1)
-        
+            elif j == -1:
+                return i + 1
+
+            if (i, j) in dp:
+                return dp[(i, j)]
+
+            if w1[i] == w2[j]:
+                dp[(i, j)] = rec(i - 1, j - 1)
+                return dp[(i, j)]
+
+            ins = rec(i, j - 1)
+            de = rec(i - 1, j)
+            rep = rec(i - 1, j - 1)
+            dp[(i, j)] = min(ins, de, rep) + 1
+            return dp[(i, j)]
+
+        return rec(len(w1) - 1, len(w2) - 1)
