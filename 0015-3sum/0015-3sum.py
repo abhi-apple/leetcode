65937d5c2f94d8fe1e3a12f1c4d541455bb32167
Abhinay <abhinayreddy0112@gmail.com>
2023-06-02 10:48:22 +0530

    Time: 8121 ms (5.88%), Space: 20.6 MB (31.29%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,19 +1,19 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
         nums.sort()
-        trip=set()
-        for i in range(len(nums)-2):
-            if i>0 and nums[i]==nums[i-1]:
-                continue
-            j=i+1
-            k=len(nums)-1
-            while j<k:
-                if nums[i]+nums[j]+nums[k]==0:
-                    trip.add((nums[i],nums[j],nums[k]))
-                    j+=1
-                    k-=1
-                elif nums[i]+nums[j]+nums[k]>0:
-                    k-=1
+        for i in range(len(nums)):
+            val = -nums[i]
+            k = i + 1
+            j = len(nums) - 1
+            while k < j:
+                if nums[k] + nums[j] == val:
+                    if [nums[i], nums[k], nums[j]] not in ans:
+                        ans.append([nums[i], nums[k], nums[j]])
+                    k += 1
+                    j -= 1
+                elif nums[k] + nums[j] > val:
+                    j -= 1
                 else:
-                    j+=1
-        return list(trip)
+                    k += 1
+        return ans
