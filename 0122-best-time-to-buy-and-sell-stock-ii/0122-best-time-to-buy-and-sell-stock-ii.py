46687417da73ad8150532ff33ae27744b64f08dd
Abhinay <abhinayreddy0112@gmail.com>
2023-06-03 20:12:05 +0530

    Time: 119 ms (5.12%), Space: 49.3 MB (5.03%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,21 +1,18 @@
 class Solution:
     def maxProfit(self, p: List[int]) -> int:
-        prof=0
-        def ans(ind,buy):
-            if ind==len(p):
+        dp={}
+        def rec(i,bol):
+            if i==len(p):
                 return 0
-            if dp[ind][buy]!=-1:
-                return dp[ind][buy]
-            if buy:
-                dp[ind][buy]=max(-p[ind]+ans(ind+1,0),ans(ind+1,1))
-                
+            if (i,bol) in dp:
+                return dp[(i,bol)]
+            prof=0
+            pr=0
+            if bol:
+                prof=max(-p[i]+rec(i+1,not bol),rec(i+1,bol))
             else:
-                dp[ind][buy]=max(p[ind]+ans(ind+1,1),ans(ind+1,0))
-      
-            return dp[ind][buy]
-                
+                pr=max(rec(i+1,bol),p[i]+rec(i+1,not bol))
             
-            
-        dp=[[-1,-1] for i in range(len(p))]
-        return ans(0,1)
-        
+            dp[(i,bol)]=max(prof,pr)
+            return dp[(i,bol)]
+        return rec(0,True)
