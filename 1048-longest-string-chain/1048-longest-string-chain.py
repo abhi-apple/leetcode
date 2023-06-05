f53bbff0dfc2da8100f6b6205870624a430bb333
Abhinay <abhinayreddy0112@gmail.com>
2023-06-05 11:39:14 +0530

    Time: 8832 ms (5.00%), Space: 114.1 MB (5.73%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -0,0 +1,47 @@
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        dp={}
+        words.sort(key=len)
+        def rec(i,pr):
+            def chk(st,com):
+                n=len(st)
+                m=len(com)
+                if m!=n+1:
+                    return False
+                i=0
+                j=0
+                chk=False
+                # print(st,com)
+                # while i<n or j<m:
+                #     if st[i]==com[j]:
+                #         i+=1
+                #         j+=1
+                #     else:
+                #         if chk:
+                #             return False
+                #         j+=1
+                #         chk=True
+                # return True
+                while i < n and j < m:
+                    if st[i] == com[j]:
+                        i += 1
+                        j += 1
+                        continue
+                    if chk:
+                        return False
+                    j += 1
+                    chk = True
+                return True
+                        
+                
+            if i==len(words):
+                return 0
+            if (i,pr) in dp:
+                return dp[(i,pr)]
+            nt=rec(i+1,pr)
+            tk=0
+            if pr==-1 or chk(words[pr],words[i]):
+                tk=1+rec(i+1,i)
+            dp[(i,pr)]=max(tk,nt)
+            return dp[(i,pr)]
+        return rec(0,-1)
