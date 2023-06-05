81ebf2d97a01f164b36f7f18dabd34cac51f22fe
Abhinay <abhinayreddy0112@gmail.com>
2023-06-05 21:15:53 +0530

    Time: 51 ms (40.62%), Space: 17.6 MB (5.15%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,19 +1,18 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        st=[]
-        res=[]
-        def bck(ope,cld):
-            if ope==cld==n:
-                res.append(''.join(st))
-                return
-            if ope<n:
-                st.append("(")
-                bck(ope+1,cld)
-                st.pop()
-            if cld<ope:
-                st.append(")")
-                bck(ope,cld+1)
-                st.pop()
-        bck(0,0)
-        return res
+        dp={}
+        main=[]
+        def rec(op,cl,cur):
+            if op==n and cl==n:
+                main.append(cur)
+                return 
+            if (op,cl,cur) in dp:
+                return dp[(op,cl,cur)]
+            if op<n:
+                rec(op+1,cl,cur+'(')
+            if cl<n and op>=cl+1:
+                rec(op,cl+1,cur+')')
+            dp[(op, cl, cur)] = True
+        rec(0,0,'')
+        return main
                 
