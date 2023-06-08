62f652166fc3a4ab90d477a797043875756f71db
Abhinay <abhinayreddy0112@gmail.com>
2023-06-08 23:09:30 +0530

    Time: 59 ms (73.62%), Space: 21.2 MB (33.38%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,29 +6,20 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def hei(node):
+        ans = True
+
+        def rec(node):
+            nonlocal ans
             if not node:
                 return 0
-            lft=hei(node.left)
-            rgt=hei(node.right)
-            if lft==-1 or rgt==-1:
-                return -1
-            if abs(lft-rgt)>1:
-                return -1
-            return max(lft,rgt)+1
-        if hei(root)==-1:
-            return False
-        return True
-            
-#         ans=True
-#         def res(root):
-#             if root==None:
-#                 return 0
-#             lh=res(root.left)+1
-#             th=res(root.right)+1
-#             if abs(lh-th)>1:
-#                 ans=False
-            
-            
-            
-#         return ans
+
+            left_height = rec(node.left)
+            right_height = rec(node.right)
+
+            if abs(left_height - right_height) > 1:
+                ans = False
+
+            return max(left_height, right_height) + 1
+
+        rec(root)
+        return ans
