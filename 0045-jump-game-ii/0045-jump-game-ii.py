891fbec0db9ebeee1c24c2aca72e43ea3cade34e
Abhinay <abhinayreddy0112@gmail.com>
2023-06-09 19:55:16 +0530

    Time: 12795 ms (5.00%), Space: 32.9 MB (5.08%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,10 +1,21 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        cur,ne=0,0
-        ret=0
-        for i in range(len(nums)-1):
-            cur=max(cur,i+nums[i])
-            if i==ne:
-                ret+=1
-                ne=cur
-        return ret
+        dp = {}  # Memoization dictionary to store calculated results
+        
+        def rec(i):
+            if i >= len(nums) - 1:
+                return 0  # Already at the last index, no need to jump
+            
+            if i in dp:
+                return dp[i]
+            
+            res = float('inf')  # Initialize result with infinity
+            
+            for j in range(1, nums[i] + 1):
+                if i + j < len(nums):
+                    res = min(res, 1 + rec(i + j))  # Calculate minimum jumps recursively
+            
+            dp[i] = res  # Store the result in memoization dictionary
+            return dp[i]
+        
+        return rec(0)
