e7045aa53ae7421a62e22a7d7ef013cc7fd5da7a
Abhinay <abhinayreddy0112@gmail.com>
2023-06-12 12:47:19 +0530

    Time: 57 ms (59.13%), Space: 19.2 MB (21.19%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,38 +6,13 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        return self.checkBST(root, float('-inf'), float('inf'))
-
-    def checkBST(self, node, min_val, max_val):
-        if node is None:
-            return True
-        if node.val <= min_val or node.val >= max_val:
-            return False
-        left_valid = self.checkBST(node.left, min_val, node.val)
-        right_valid = self.checkBST(node.right, node.val, max_val)
-        return left_valid and right_valid
-
-# class Solution:
-#     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-#         if not root:
-#             return True
-#         lft=self.isValidBST(root.left)
-#         rgt=self.isValidBST(root.right)
-#         if root.left and root.right:
-#             if root.left.val<root.val and root.right.val>root.val:
-#                 return True
-#             else:
-#                 return False
-#         elif root.left and not root.right:
-#             return False
-#         elif not root.left and root.right:
-#             return False
-#         else:
-#             return True
-        # if root.right:
-        #     if root.right.val>root.val:
-        #         print( root.right.val,root.val)
-        #         return True
-        #     else:
-        #         return False
-        return lft and rgt
+        def rec(node,maxi,mini):
+            if not node:
+                return True
+            lft=rec(node.left,node.val,mini)
+            rgt=rec(node.right,maxi,node.val)
+            if node.val<maxi and node.val>mini:
+                return lft and rgt
+            else:
+                return False
+        return rec(root,float('inf'),-float('inf'))
