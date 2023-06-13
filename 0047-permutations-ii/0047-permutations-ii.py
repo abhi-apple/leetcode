49ecbac890e182f2f5d5246f287278670a4ed64c
Abhinay <abhinayreddy0112@gmail.com>
2023-06-13 10:11:45 +0530

    Time: 91 ms (31.35%), Space: 16.6 MB (63.63%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,14 +1,16 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        ans=[]
-        def f(ind,nums,ans):
-            if ind==len(nums):
-                if nums not in ans:
-                    ans.append(nums.copy())
-                return
-            for i in range(ind,len(nums)):
-                nums[i],nums[ind]=nums[ind],nums[i]
-                f(ind+1,nums,ans)
-                nums[i],nums[ind]=nums[ind],nums[i]
-        f(0,nums,ans)
-        return ans
+        fin=[]
+        cnt=Counter(nums)
+        def dfs(arr):
+            nonlocal fin
+            if len(arr)==len(nums):
+                fin.append(arr)
+                return 
+            for n in cnt:
+                if cnt[n]>0:
+                    cnt[n]-=1
+                    dfs(arr+[n])
+                    cnt[n]+=1
+        dfs([])
+        return fin
