54e8b0a616bbba7f03117ec1c1091fb5d7c58931
Abhinay <abhinayreddy0112@gmail.com>
2023-06-13 22:44:35 +0530

    Time: 726 ms (35.91%), Space: 20.9 MB (7.40%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,29 +1,70 @@
+from typing import List
+from collections import deque
+
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        n=len(mat)
-        m=len(mat[0])
-        vis=[[1 for i in range(m)] for j in range(n)]
-        dist=[[1 for i in range(m)] for j in range(n)]
-        que=[]
-        for i in range(n):
-            for j in range(m):
+        m, n = len(mat), len(mat[0])
+        ans = [[0 for _ in range(n)] for _ in range(m)]
+        vis=set()
+        que=deque()
+        for i in range(m):
+            for j in range(n):
                 if mat[i][j]==0:
-                    que.append([i,j,0])
-                    vis[i][j]=0
-                else:
-                    vis[i][j]=1
-        drow=[-1,0,1,0]
-        dcol=[0,1,0,-1]
-        
+                    que.append((i,j,0))
         while que:
-            row,col,step=que.pop(0)
-            dist[row][col]=step
-            for i in range(4):
-                nrow=row+drow[i]
-                ncol=col+dcol[i]
-                if nrow>=0 and nrow<n and ncol>=0 and ncol<m and vis[nrow][ncol]==1:
-                    vis[nrow][ncol]=0
-                    que.append([nrow,ncol,step+1])
-                    
-        return dist
+            k=len(que)
+            for _ in range(k):
+                i,j,tim=que.popleft()
+                dire = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+                for d in dire:
+                    ix,jx=i+d[0],j+d[1]
+                    if 0 <= ix < m and 0 <= jx < n:
+                        # print(ix,jx,m,n)
+                        if mat[ix][jx] == 1 and (ix, jx) not in vis:
+                            que.append((ix, jx, tim + 1))
+                            ans[ix][jx]=tim+1
+                            vis.add((ix, jx))
+                                
+                                
             
+
+        return ans
+            
+        
+#         def bfs(i, j):
+#             vis = set()
+#             vis.add((i, j))
+#             que = deque([(i, j, 0)])
+            
+#             while que:
+#                 size = len(que)
+#                 mini=-float(inf)
+                
+#                 for _ in range(size):
+#                     i, j, tim = que.popleft()
+                    
+                    
+                    
+#                     directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+#                     for d in directions:
+#                         ix, jx = i + d[0], j + d[1]
+#                         if 0 <= ix < m and 0 <= jx < n:
+#                             if mat[ix][jx] == 1 and (ix, jx) not in vis:
+#                                 que.append((ix, jx, tim + 1))
+#                                 vis.add((ix, jx))
+#                             elif mat[ix][jx] == 0:
+#                                 mini=min(mini,tim+1)
+#                                 dp[(ix, jx)] = tim + 1
+#                                 return tim + 1
+#                     dp[(i,j)]=mini
+#                     return dp[(i,j)]
+#             return 0
+        
+#         for i in range(m):
+#             for j in range(n):
+#                 if mat[i][j] == 1:
+#                     ans[i][j] = bfs(i, j)
+#                     dp[(i, j)] = ans[i][j]
+                  
+#         return ans
+
