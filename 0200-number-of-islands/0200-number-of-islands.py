2442e6498f376f2afe5cc100b5046b02f5a469ca
Abhinay <abhinayreddy0112@gmail.com>
2023-06-13 12:14:32 +0530

    Time: 386 ms (26.15%), Space: 28.3 MB (5.15%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,23 +4,19 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         cnt=0
         vis=set()
-        def bfs(i,j):
+        def dfs(i,j):
             vis.add((i,j))
-            que = deque([(i, j)])
-            while que:
-                n,p=que.popleft()
-                dire = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-                for d in dire:
-                    nx,px=n+d[0],p+d[1]
-                    if 0<=nx<len(grid) and 0<=px<len(grid[0]) and (nx,px) not in vis and grid[nx][px]=='1':
-                        que.append((nx,px))
-                        vis.add((nx,px))
-        
+            dire = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+            for d in dire:
+                ix,jx=i+d[0],j+d[1]
+                if 0<=ix<len(grid) and 0<=jx<len(grid[0]) and (ix,jx) not in vis and grid[ix][jx]=='1':
+                    dfs(ix,jx)
+                
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if (i,j) not in vis and grid[i][j]=='1':
-                    bfs(i,j)
+                    dfs(i,j)
                     # print(cnt)
                     cnt+=1
         return cnt
