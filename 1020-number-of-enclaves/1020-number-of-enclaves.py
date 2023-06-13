cd36d733ee2dc6c1a2a36950b41b0e1b00489eed
Abhinay <abhinayreddy0112@gmail.com>
2023-06-14 00:46:43 +0530

    Time: 731 ms (46.34%), Space: 96.8 MB (25.53%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,36 +1,30 @@
 class Solution:
-    def numEnclaves(self, mat: List[List[int]]) -> int:
-        def dfs(row,col):
-            vis[row][col]=1
-            n=len(mat)
-            m=len(mat[0])
-            for i in range(4):
-                nr=row+drow[i]
-                nc=col+dcol[i]
-                if nr>=0 and nr<n and nc>=0 and nc<m and not vis[nr][nc] and mat[nr][nc]==1:
-                    dfs(nr,nc)
-                    
-        n=len(mat)
-        m=len(mat[0])
-        drow=[-1,0,1,0]
-        dcol=[0,1,0,-1]
+    def numEnclaves(self, board: List[List[int]]) -> int:
+        vis=set()
         cnt=0
-        vis=[[0 for j in range(m)] for i in range(n)]
-        
-        for j in range(m):
-            if not vis[0][j] and mat[0][j]==1:
-                dfs(0,j)
-            if not vis[n-1][j] and mat[n-1][j]==1:
-                dfs(n-1,j)
-        
-        for i in range(n):
-            if not vis[i][0] and mat[i][0]==1:
+        def dfs(i,j):
+            vis.add((i,j))
+            dire= [(-1, 0), (0, 1), (1, 0), (0, -1)]
+            for d in dire:
+                ix,jx=i+d[0],j+d[1]
+                if 0 <= ix < len(board) and 0 <= jx < len(board[0]) and (ix, jx) not in vis and board[ix][jx]==1:
+                    dfs(ix,jx)
+                    
+        for i in range(len(board)):
+            if i==0:
+                for k in range(len(board[0])):
+                    if board[0][k]==1:
+                        dfs(0,k)
+            if board[i][0]==1:
                 dfs(i,0)
-            if not vis[i][m-1] and mat[i][m-1]==1:
-                dfs(i,m-1)
-        for i in range(n):
-            for j in range(m):
-                if not vis[i][j] and mat[i][j]==1:
+            if board[i][-1]==1:
+                dfs(i,len(board[0])-1)
+            if i==len(board)-1:
+                for k in range(len(board[0])):
+                    if board[i][k]==1:
+                        dfs(i,k)
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if (i,j) not in vis and board[i][j]==1:
                     cnt+=1
         return cnt
-                
