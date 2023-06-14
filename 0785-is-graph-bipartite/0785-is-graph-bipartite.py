bc7c4a642e5c41424a80e2350e99bd54dfac176d
Abhinay <abhinayreddy0112@gmail.com>
2023-06-14 11:41:18 +0530

    Time: 232 ms (6.73%), Space: 17.2 MB (8.27%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,29 +1,27 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        n = len(graph)
-        col=[-1]*n
-        
-        def bfs(ind):
-            que=deque()
-            que.append(ind)
-            col[ind]=1
-            while que:
-                print(col)
-                val=que.popleft()
-            
-                for i in graph[val]:
-                    if col[i]==-1:
-                        col[i]= not col[val]
-                        que.append(i)
-                    elif col[i]==col[val]:
-                        print(i,val)
-                        return False
+        adj={}
+        dic={}
+        for i in range(len(graph)):
+            adj[i]=[]
+            for j in graph[i]:
+                adj[i].append(j)
+                
+        def dfs(node,col):
+            if node in dic:
+                return dic[node]==col
+            dic[node]=col
+            for var in adj[node]:
+                if not dfs(var,not col):
+                    return False
             return True
-            
-        for i in range(len(col)):
-            if col[i]==-1:
-                if not bfs(i):
-                    print(i)
+                
+        # return dfs(0,True)
+        for i in range(len(graph)):
+            if i not in dic:
+                if not dfs(i, True):
                     return False
         return True
-    
+                    
+                    
+            
