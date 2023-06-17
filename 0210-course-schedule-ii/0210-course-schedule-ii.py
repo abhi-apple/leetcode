1e12f8165fa503973d0c0b2a6d453f1086fbc832
Abhinay <abhinayreddy0112@gmail.com>
2023-06-17 10:15:25 +0530

    Time: 115 ms (63.75%), Space: 18 MB (60.52%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,27 +1,23 @@
 class Solution:
-    def findOrder(self, numCourses: int, p: List[List[int]]) -> List[int]:
-        maps={i:[] for i in range(numCourses)}
-        for crs , pre in p:
-            maps[crs].append(pre)
-        vis=set()
-        cycle=set()
-        
-        res=[]
-        def dfs(crs):
-            if crs in cycle:
-                return False
-            if crs in vis:
-                return True
-            cycle.add(crs)
-            for pre in maps[crs]:
-                if dfs(pre)==False:
-                    return False
-            cycle.remove(crs)
-            vis.add(crs)
-            res.append(crs)
-            return True
-        for c in range(numCourses):
-            if dfs(c)==False:
-                return []
-        return res
+    def findOrder(self, nums: int, p: List[List[int]]) -> List[int]:
+        adj={i:[] for i in range(nums)}
+        ind = [0] * nums
+        for i in p:
+            adj[i[1]].append(i[0])
+            ind[i[0]]+=1
+        que=deque()
+        for i in range(nums):
+            if ind[i]==0:
+                que.append(i)
+        top=[]
+        while que:
+            var=que.popleft()
+            top.append(var)
+            for i in adj[var]:
+                ind[i]-=1
+                if ind[i]==0:
+                    que.append(i)
+        if len(top)==nums:
+            return top
+        return []
             
