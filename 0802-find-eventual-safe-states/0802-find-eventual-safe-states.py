677c3d71d250a7ed17dc1ab1f2d38d00be608fd0
Abhinay <abhinayreddy0112@gmail.com>
2023-06-17 10:40:07 +0530

    Time: 720 ms (24.57%), Space: 24 MB (50.87%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,38 +1,22 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        ans = []
-        dic = {}
-        for i in range(len(graph)):
-            dic[i] = []
+        n=len(graph)
+        adj={i:[] for i in range(n)}
+        ind=[0]*n
+        for i in range(n):
             for k in graph[i]:
-                dic[i].append(k)
-            if dic[i] == []:
-                ans.append(i)
-        
-        def dfs(node, st, vis, memo):
-            if node in vis:
-                return False
-            if node in memo:
-                return memo[node]
-
-            vis.add(node)
-            for var in dic[node]:
-                if not dfs(var, st, vis, memo):
-                    memo[node] = False
-                    return False
-
-            vis.remove(node)
-            memo[node] = True
-            return True
-
-        vis = set()
-        memo = {}
-        for i in dic:
-            if dic[i] != []:
-                st = i
-                if i not in vis and dfs(i, st, vis, memo):
-                    ans.append(i)
-
-        return sorted(ans)
-
-                
+                adj[k].append(i)
+                ind[i]+=1
+        que=deque()
+        for i in range(n):
+            if ind[i]==0:
+                que.append(i)
+        res=[]
+        while que:
+            var=que.popleft()
+            res.append(var)
+            for va in adj[var]:
+                ind[va]-=1
+                if ind[va]==0:
+                    que.append(va)
+        return sorted(res)
