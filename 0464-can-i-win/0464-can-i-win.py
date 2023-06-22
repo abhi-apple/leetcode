28c89f4362fc768d0152602c37e8b9c04da9539b
Abhinay <abhinayreddy0112@gmail.com>
2023-06-22 13:13:19 +0530

    Time: 3257 ms (47.64%), Space: 150.9 MB (60.73%) - LeetHub

--- 0464-can-i-win/0464-can-i-win.py
+++ 0464-can-i-win/0464-can-i-win.py
@@ -3,13 +3,19 @@ class Solution:
         cad = list(range(1, maxch + 1))
         if sum(cad) < tot:
             return False
-        @cache
+        # @cache
+        dp={}
         def rec(arr,sums):
             if arr[-1]>=sums:
                 return True
+            if (arr,sums) in dp:
+                return dp[(arr,sums)]
+           
             for i in range(len(arr)):
                 if not rec(arr[:i]+arr[i+1:],sums-arr[i]):
+                    dp[(arr,sums)]=True
                     return True
+            dp[(arr,sums)]=False
             return False
         return rec(tuple(cad),tot)
             
