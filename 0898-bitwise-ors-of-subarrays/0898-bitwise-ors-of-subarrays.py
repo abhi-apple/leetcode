c23ce9294f2e017cfe6d340fe84434bdb504f22c
Abhinay <abhinayreddy0112@gmail.com>
2023-06-22 23:34:15 +0530

    Time: 2329 ms (5.26%), Space: 620.1 MB (5.26%) - LeetHub

--- 0898-bitwise-ors-of-subarrays/0898-bitwise-ors-of-subarrays.py
+++ 0898-bitwise-ors-of-subarrays/0898-bitwise-ors-of-subarrays.py
@@ -1,26 +1,19 @@
 class Solution:
     def subarrayBitwiseORs(self, arr: List[int]) -> int:
-        fin = set()
-        n = len(arr)
-        dp = {}
-
-        def rec(i, ors):
-            if (i, ors) in dp:
-                return dp[(i, ors)]
-
-            if ors not in fin:
-                fin.add(ors)
-
-            if i == n:
-                return
-
-            rec(i + 1, ors | arr[i])
-            dp[(i, ors)] = None  # Store the result to avoid recomputation
-            return
-
+        fin=set()
+        n=len(arr)
+        dp={}
+        def rec(i,ors):
+            if (i,ors) in dp:
+                return dp[(i,ors)]
+            fin.add(ors)
+            if i==n:
+                return 
+            
+            rec(i+1,ors | arr[i])
+            dp[(i,ors)]=None
+            return 
         for i in range(n):
-            rec(i, arr[i])
-
+            rec(i,arr[i])
         return len(fin)
-
             
