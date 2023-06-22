02cce51e44d449189d2e4a398c7049af75bf9b3d
Abhinay <abhinayreddy0112@gmail.com>
2023-06-22 23:57:25 +0530

    Time: 5705 ms (14.18%), Space: 97 MB (6.28%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -0,0 +1,51 @@
+# class Solution:
+#     def knightDialer(self, n: int) -> int:
+#         main=0
+#         @cache
+
+#         def rec(i,j,cnt):
+#             nonlocal main
+#             if cnt==0:
+#                 main+=1
+#                 return 
+#             dire=[(-1,2),(1,2),(-1,-2),(1,-2),(-2,-1),(-2,1),(2,-1),(2,1)]
+#             for d in dire:
+#                 ix,jx=i+d[0],j+d[1]
+#                 if 0<=ix<4 and 0<=jx<3 and (ix,jx)!=(3,0) and (ix,jx)!=(3,2):
+#                     rec(ix,jx,cnt-1)
+#             return 
+#         for i in range(3):
+#             for j in range(3):
+#                 rec(i,j,n-1)
+#         rec(3,1,n-1)
+#         return main
+from functools import cache
+
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        count = 0
+        
+        @cache
+        def rec(i, j, cnt):
+            nonlocal count
+            
+            if cnt == 0:
+                return 1
+            
+            dire = [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)]
+            total = 0
+            
+            for d in dire:
+                ix, jx = i + d[0], j + d[1]
+                
+                if 0 <= ix < 4 and 0 <= jx < 3 and (ix, jx) != (3, 0) and (ix, jx) != (3, 2):
+                    total += rec(ix, jx, cnt - 1)
+            
+            return total
+        
+        for i in range(3):
+            for j in range(3):
+                count += rec(i, j, n - 1)
+        count+=rec(3,1,n-1)
+        
+        return count % (10**9 + 7)
