0a18a2b1f897b0cfb92f9cf07c0b0b63e45701f4
Abhinay <abhinayreddy0112@gmail.com>
2023-06-27 13:21:50 +0530

    Time: 563 ms (96.44%), Space: 22.2 MB (72.76%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,23 +1,24 @@
 class Solution:
     def shipWithinDays(self, w: List[int], da: int) -> int:
-        low=max(w)
-        hig=sum(w)
-        ans=0
-        while low<=hig:
-            def istr(mid,da):
-                d=1
-                sums=0
-                for i in range(len(w)):
-                    sums+=w[i]
-                    if sums>mid:
-                        d+=1
-                        sums=w[i]
-                return True if d<=da else False
-            mid=low+(hig-low)//2
-            if (istr(mid,da)):
-                ans=mid
-                hig=mid-1
+        i = max(w)
+        j = sum(w)
+
+        def rec(val):
+            days = 1
+            total = 0
+            for weight in w:
+                total += weight
+                if total > val:
+                    days += 1
+                    total = weight
+            return days
+
+        fin = 0
+        while i <= j:
+            mid = (i + j) // 2
+            if rec(mid) <= da:
+                fin = mid
+                j = mid - 1
             else:
-                low=mid+1
-        return ans
-                
+                i = mid + 1
+        return fin
