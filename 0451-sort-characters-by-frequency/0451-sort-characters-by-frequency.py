3974110404a28b5d0f92b5745b5fb747f40b2757
Abhinay <abhinayreddy0112@gmail.com>
2023-07-04 18:25:29 +0530

    Time: 57 ms (75.13%), Space: 17.7 MB (41.78%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,9 +1,14 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        otp=[]
-        for i in set(s):
-            cnt=s.count(i)
-            otp.append(i*cnt)
-            
-        s=sorted(otp,key=len,reverse=True)
-        return "".join(s)
+        dic={}
+        for i in s:
+            if i in dic:
+                dic[i]+=1
+            else:
+                dic[i]=1
+        
+        fin=sorted(dic.items(), key=lambda x:x[1],reverse=True)
+        res=''
+        for st,val in fin:
+            res=res+st*val
+        return res
