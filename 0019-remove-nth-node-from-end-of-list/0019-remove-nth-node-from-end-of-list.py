b5914ee27b46b6a4d122437d8882a28cad930827
Abhinay <abhinayreddy0112@gmail.com>
2023-07-05 12:15:35 +0530

    Time: 54 ms (27.89%), Space: 16.3 MB (35.91%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -3,17 +3,34 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+#         dumm=ListNode(0)
+#         dumm.next=head
+#         lft=dumm
+#         rgt=dumm.next
+#         while n<0 and rgt:
+#             rgt=rgt.next
+#             n-=1
+#         while rgt:
+#             lft=lft.next
+#             rgt=rgt.next
+            
+#         lft.next=lft.next.next
+#         return dumm.next
+
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        dum=ListNode(0,head)
-        lef=dum
-        rig=dum.next
-        while n>0 and rig:
-            rig=rig.next
-            n-=1
-        while rig:
-            lef=lef.next
-            rig=rig.next
-        lef.next=lef.next.next
-        return dum.next
-            
+        dumm = ListNode(0)
+        dumm.next = head
+        lft = dumm
+        rgt = dumm.next
+        while n > 0 and rgt:
+            rgt = rgt.next
+            n -= 1
+        while rgt:
+            lft = lft.next
+            rgt = rgt.next
+        lft.next = lft.next.next
+        return dumm.next
+
