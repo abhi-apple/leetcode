3e7768f56c9f1977304bf6a28ce467d1ff014eed
Abhinay <abhinayreddy0112@gmail.com>
2023-07-07 18:42:36 +0530

    Time: 3659 ms (29.17%), Space: 16.6 MB (47.02%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -1,19 +1,11 @@
-from functools import cache
-from typing import List
-
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
-        sums = 0
-        def rec(i, mini, maxi):
-            nonlocal sums
-            if i == len(nums):
-                sums += (maxi - mini)
-                return
-            if mini != float('inf'):
-                sums += (maxi - mini)
-            rec(i + 1, min(mini, nums[i]), max(maxi, nums[i]))
-
+        sums=0
         for i in range(len(nums)):
-            rec(i, float('inf'), -float('inf'))
-
+            maxi=nums[i]
+            mini=nums[i]
+            for j in range(i+1,len(nums)):
+                maxi=max(maxi,nums[j])
+                mini=min(mini,nums[j])
+                sums+=(maxi-mini)
         return sums
