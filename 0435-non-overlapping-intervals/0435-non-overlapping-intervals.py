e15ded7ddaa4a7784b257f18a2a92cebdaae7d6b
Abhinay <abhinayreddy0112@gmail.com>
2023-07-12 19:57:09 +0530

    Time: 1610 ms (8.40%), Space: 55.4 MB (46.88%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -1,33 +1,13 @@
-# class Solution:
-#     def eraseOverlapIntervals(self, inte: List[List[int]]) -> int:
-#         inte.sort(key=lambda x: x[0])
-#         dp={}
-#         n=len(inte)
-#         dp=[[-1 for i in range(n)] for j in range(n)]
-#         def rec(i,pr):
-#             if i==n:
-#                 return 0
-#             if dp[i][pr]!=-1:
-#                 return dp[i][pr]
-#             nt=rec(i+1,pr)
-#             tk=0
-#             if pr==-1 or inte[i][0]>=inte[pr][1]:
-#                 tk=1+rec(i+1,i)
-#             dp[i][pr]=max(nt,tk)
-#             return dp[i][pr]
-#         return n-rec(0,-1)
 class Solution:
     def eraseOverlapIntervals(self, inte: List[List[int]]) -> int:
-        inte.sort(key=lambda x: x[1])
-
-        n = len(inte)
-        if n <= 1:
-            return 0
-
-        cnt = 1
-        end = inte[0][1]  # Initialize the end with the end time of the first interval
+        inte.sort()
+        st=inte[0][1]
+        cnt=0
         for i in inte[1:]:
-            if i[0]>=end:
+            if i[0]<st:
+                
                 cnt+=1
-                end=i[1]
-        return n-cnt
+                st=min(st,i[1])
+            else:
+                st=i[1]
+        return cnt
