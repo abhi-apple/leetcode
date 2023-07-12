c140f9bb88ce654ba2954aefe1dc5306106e5464
Abhinay <abhinayreddy0112@gmail.com>
2023-07-12 11:28:13 +0530

    Time: 227 ms (37.80%), Space: 18 MB (76.04%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,16 +1,35 @@
+from typing import List
+from collections import Counter
+
 class Solution:
-    def isNStraightHand(self, hand: List[int], k: int) -> bool:
-        if len(hand) % k != 0:
-            return False
-        cnt=Counter(hand)
-        ans=sorted(cnt)
-        while ans:
-            st=ans[0]
-            for i in range(st,st+k):
-                if cnt[i]==0:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        hand.sort()
+        count = Counter(hand)
+        
+        for i in range(len(hand)):
+            if hand[i] in count:
+                val=hand[i]
+                sz = 0
+                
+                while  val  in count and sz < groupSize:
+                    count[val] -= 1
+                    if count[val] == 0:
+                        del count[val]
+                    sz += 1
+                    val+=1
+                    
+                
+                # Check if all required cards were found
+                # print(count,sz)
+                if sz != groupSize:
                     return False
-                cnt[i]-=1
-                if cnt[i]==0:
-                    ans.remove(i)
+        
+        # Check if there are any remaining cards
+        if count:
+            return False
+        
         return True
-            
+
+                    
+                    
+                
