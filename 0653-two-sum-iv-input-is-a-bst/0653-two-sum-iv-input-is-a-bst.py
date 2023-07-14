95d3db45fb2cda4a0757ef11dcc2ff8380b78e94
Abhinay <abhinayreddy0112@gmail.com>
2023-07-14 12:35:48 +0530

    Time: 97 ms (48.19%), Space: 22.9 MB (11.59%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -6,39 +6,17 @@
 #         self.right = right
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        elem=[]
-        def inor(node):
+        store=set()
+        def rec(node):
+            nonlocal store
             if not node:
-                return 
-            inor(node.left)
-            elem.append(node.val)
-            inor(node.right)
-        inor(root)
-        left=0
-        rgt=len(elem)-1
-        while left<rgt:
-            if elem[left]+elem[rgt]==k:
+                return False
+            lft=rec(node.left)
+            rgt=rec(node.right)
+            if k-node.val in store:
                 return True
-            elif elem[left]+elem[rgt]<k:
-                left+=1
-            else:
-                rgt-=1
-        return False
-#         def appendValuesToList(root, values):
-#             if not root:
-#                 return
-
-#             values.append(root.val)
-
-#             appendValuesToList(root.left, values)
-#             appendValuesToList(root.right, values)
-#         values=[]
-#         appendValuesToList(root,values)
-#         seen=set()
-#         for i in values:
-#             if k-i in seen:
-#                 return True
-#             seen.add(i)
-#         return False
-
-        
+            store.add(node.val)
+            return lft or rgt
+        return rec(root)
+            
+            
