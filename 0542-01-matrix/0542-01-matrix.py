9091b09b2745a6981aa374b8d0b06f5856cbd7d6
Abhinay <abhinayreddy0112@gmail.com>
2023-07-15 11:33:52 +0530

    Time: 640 ms (71.15%), Space: 20 MB (42.42%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,70 +1,52 @@
-from typing import List
+# class Solution:
+#     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+#         n=len(mat)
+#         m=len(mat[0])
+#         res=[[0 for i in range(m)] for j in range(n)]
+#         stack=deque()
+#         for i in range(n):
+#             for j in range(m):
+#                 if mat[i][j]==0:
+#                     stack.append((i,j))
+#         cnt=1
+#         dire=[(0,1),(1,0),(-1,0),(0,-1)]
+#         # vis=set()
+#         while stack:
+#             n=len(stack)
+#             for _ in range(n):
+#                 i,j=stack.popleft()
+#                 for d in dire:
+#                     ix,jx=i+d[0],j+d[1]
+#                     print(ix,jx)
+#                     if 0<=ix<n and 0<=jx<m and mat[ix][jx]==1:
+#                         res[ix][jx]=cnt
+#                         mat[ix][jx]=0
+#                         stack.append((ix,jx))
+#         return mat
+                
+            
 from collections import deque
-
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        m, n = len(mat), len(mat[0])
-        ans = [[0 for _ in range(n)] for _ in range(m)]
-        vis=set()
-        que=deque()
-        for i in range(m):
-            for j in range(n):
-                if mat[i][j]==0:
-                    que.append((i,j,0))
-        while que:
-            k=len(que)
-            for _ in range(k):
-                i,j,tim=que.popleft()
-                dire = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        n = len(mat)
+        m = len(mat[0])
+        res = [[0 for _ in range(m)] for _ in range(n)]
+        stack = deque()
+        for i in range(n):
+            for j in range(m):
+                if mat[i][j] == 0:
+                    stack.append((i, j))
+        cnt = 1
+        dire = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        while stack:
+            size = len(stack)
+            for _ in range(size):
+                i, j = stack.popleft()
                 for d in dire:
-                    ix,jx=i+d[0],j+d[1]
-                    if 0 <= ix < m and 0 <= jx < n:
-                        # print(ix,jx,m,n)
-                        if mat[ix][jx] == 1 and (ix, jx) not in vis:
-                            que.append((ix, jx, tim + 1))
-                            ans[ix][jx]=tim+1
-                            vis.add((ix, jx))
-                                
-                                
-            
-
-        return ans
-            
-        
-#         def bfs(i, j):
-#             vis = set()
-#             vis.add((i, j))
-#             que = deque([(i, j, 0)])
-            
-#             while que:
-#                 size = len(que)
-#                 mini=-float(inf)
-                
-#                 for _ in range(size):
-#                     i, j, tim = que.popleft()
-                    
-                    
-                    
-#                     directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-#                     for d in directions:
-#                         ix, jx = i + d[0], j + d[1]
-#                         if 0 <= ix < m and 0 <= jx < n:
-#                             if mat[ix][jx] == 1 and (ix, jx) not in vis:
-#                                 que.append((ix, jx, tim + 1))
-#                                 vis.add((ix, jx))
-#                             elif mat[ix][jx] == 0:
-#                                 mini=min(mini,tim+1)
-#                                 dp[(ix, jx)] = tim + 1
-#                                 return tim + 1
-#                     dp[(i,j)]=mini
-#                     return dp[(i,j)]
-#             return 0
-        
-#         for i in range(m):
-#             for j in range(n):
-#                 if mat[i][j] == 1:
-#                     ans[i][j] = bfs(i, j)
-#                     dp[(i, j)] = ans[i][j]
-                  
-#         return ans
-
+                    ix, jx = i + d[0], j + d[1]
+                    if 0 <= ix < n and 0 <= jx < m and mat[ix][jx] == 1:
+                        res[ix][jx] = cnt
+                        mat[ix][jx] = 0
+                        stack.append((ix, jx))
+            cnt += 1
+        return res
