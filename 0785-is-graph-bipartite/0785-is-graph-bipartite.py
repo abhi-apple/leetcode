da457b785aa0e40f7066341179188c5e4c310f98
Abhinay <abhinayreddy0112@gmail.com>
2023-07-15 13:42:33 +0530

    Time: 187 ms (56.52%), Space: 17.1 MB (26.90%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,27 +1,53 @@
+# class Solution:
+#     def isBipartite(self, graph: List[List[int]]) -> bool:
+#         adj={}
+#         for i in range(len(graph)):
+#             adj[i]=[]
+#             for k in graph[i]:
+#                 adj[i].append(k)
+#         stack=deque()
+#         coladj={}
+#         def rec(i,colad):
+#             print(i,colad)
+#             stack.append((i,colad))
+#             while stack:
+#                 node,col=stack.popleft()
+#                 coladj[node]=col
+#                 for var in adj[node]:
+#                     if var not in coladj:
+#                         stack.append((var,not col))
+#                     else:
+#                         if coladj[var]==col:
+#                             return False
+#         for i in range(len(graph)):
+#             if i not in coladj:
+#                 rec(i,True)
+                
+#         return True
+
+from collections import deque
+
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        adj={}
-        dic={}
-        for i in range(len(graph)):
-            adj[i]=[]
-            for j in graph[i]:
-                adj[i].append(j)
-                
-        def dfs(node,col):
-            if node in dic:
-                return dic[node]==col
-            dic[node]=col
-            for var in adj[node]:
-                if not dfs(var,not col):
+        coladj = {}
+        
+        def dfs(node, col):
+            if node in coladj:
+                return coladj[node] == col
+            
+            coladj[node] = col
+            for neighbor in graph[node]:
+                if not dfs(neighbor, not col):
                     return False
             return True
-                
-        # return dfs(0,True)
+
         for i in range(len(graph)):
-            if i not in dic:
+            if i not in coladj:
                 if not dfs(i, True):
                     return False
+
         return True
-                    
-                    
-            
+
+                
+                
+                
