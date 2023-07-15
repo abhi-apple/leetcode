ac74209abb733ec964a5ebcfadadcd562d10f870
Abhinay <abhinayreddy0112@gmail.com>
2023-07-15 19:58:38 +0530

    Time: 105 ms (93.43%), Space: 17.8 MB (78.40%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,29 +1,24 @@
-
-
-from typing import List
-
 class Solution:
-    def canFinish(self, numCourses: int, pre: List[List[int]]) -> bool:
-        adj = [[] for _ in range(numCourses)]
-        indegree = [0] * numCourses
+    def canFinish(self, numc: int, pre: List[List[int]]) -> bool:
+        adj = [[] for _ in range(numc)]
+        indegree = [0] * numc
 
         for it in pre:
             adj[it[1]].append(it[0])
             indegree[it[0]] += 1
-        que=deque()
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                que.append(i)
-                    
-        top=0
-        while que:
-            node=que.popleft()
-            top+=1
-            for it in adj[node]:
-                indegree[it]-=1
-                if indegree[it]==0:
-                    que.append(it)
-        if top==numCourses:
+        stack=deque()
+        for i in range(numc):
+            if indegree[i]==0:
+                
+                stack.append(i)
+        cnt=0
+        while stack:
+            node=stack.popleft()
+            cnt+=1
+            for var in adj[node]:
+                indegree[var]-=1
+                if indegree[var]==0:
+                    stack.append(var)
+        if cnt==numc:
             return True
         return False
-      
