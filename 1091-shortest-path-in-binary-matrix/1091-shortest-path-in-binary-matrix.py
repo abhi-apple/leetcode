eff295ac515c7b15077723419a34b74337ce9022
Abhinay <abhinayreddy0112@gmail.com>
2023-07-16 23:30:25 +0530

    Time: 698 ms (36.03%), Space: 17 MB (57.66%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -5,17 +5,18 @@ class Solution:
             return -1
         dist=[[float('inf') for i in range(n)] for j in range(n)]
         
-        stack=deque([(1,0,0)])
+        stack=[]
+        heapq.heappush(stack,(1,0,0))
         dire=[(0,1),(1,0),(1,1),(-1,0),(0,-1),(-1,-1),(1,-1),(-1,1)]
         while stack:
-            patl,i,j=stack.popleft()
+            patl,i,j=heapq.heappop(stack)
             if i==n-1 and j==n-1:
                 return patl
             for d in dire:
                 ix,jx=i+d[0],j+d[1]
                 if 0<=ix<n and 0<=jx<n and grid[ix][jx]==0 and patl+1<dist[ix][jx]:
                     dist[ix][jx]=patl+1
-                    stack.append((patl+1,ix,jx))
+                    heapq.heappush(stack,(patl+1,ix,jx))
         return -1
                 
                 
