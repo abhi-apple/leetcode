6e064e1218349581f9169329963b9b7f8d4658ed
Abhinay <abhinayreddy0112@gmail.com>
2023-07-20 22:49:24 +0530

    Time: 127 ms (78.52%), Space: 18.7 MB (85.69%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,24 +1,21 @@
 class Solution:
-    def minDistance(self, w1: str, w2: str) -> int:
-        dp = {}
-
-        def rec(i, j):
-            if i == -1 :
-                return j+1
-            elif j == -1:
-                return i + 1
-
-            if (i, j) in dp:
-                return dp[(i, j)]
-
-            if w1[i] == w2[j]:
-                dp[(i, j)] = rec(i - 1, j - 1)
-                return dp[(i, j)]
-
-            ins = rec(i, j - 1)
-            de = rec(i - 1, j)
-            rep = rec(i - 1, j - 1)
-            dp[(i, j)] = min(ins, de, rep) + 1
-            return dp[(i, j)]
-
-        return rec(len(w1) - 1, len(w2) - 1)
+    def minDistance(self, word1: str, word2: str) -> int:
+        dp={}
+        n=len(word1)
+        m=len(word2)
+        def rec(i,j):
+            if i==n and j==m:
+                return 0
+            if i==n:
+                return m-j
+            if j==m:
+                return n-i
+            if (i,j) in dp:
+                return dp[(i,j)]
+            if word1[i]==word2[j]:
+                dp[(i,j)]=rec(i+1,j+1)
+            else:
+                dp[(i,j)]=min(rec(i+1,j+1),rec(i+1,j),rec(i,j+1))+1
+            return dp[(i,j)]
+        return rec(0,0)
+                
