e25bc3cbcd9ae00f2e9092a4c72010d43c0a519b
Abhinay <abhinayreddy0112@gmail.com>
2023-07-21 15:00:24 +0530

    Time: 5287 ms (20.64%), Space: 16.2 MB (94.01%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,14 +1,38 @@
-from math import sqrt
+# class Solution:
+#     def numSquares(self, n: int) -> int:
+#         nums=[]
+#         val=1
+#         while val*val<=10**4:
+#             nums.append(val*val)
+#             val+=1
+#         dp={}
+#         def rec(i,value):
+#             if value==0:
+#                 return 0
+#             if value<0 or i<=-1:
+#                 return float('inf')
+#             dp[(i,value)]=float('inf')
+#             if value-nums[i]>=0:
+#                 dp[(i,value)]=min(rec(i,value-nums[i])+1,dp[(i,value)])
+#             dp[(i,value)]=min(dp[(i,value)],rec(i-1,value))
+#             return dp[(i,value)]
+#         return rec(len(nums)-1,n)
+
 
 class Solution:
     def numSquares(self, n: int) -> int:
+        nums = []
+        val = 1
+        while val * val <= 10 ** 4:
+            nums.append(val * val)
+            val += 1
+
         dp = [float('inf')] * (n + 1)
         dp[0] = 0
-        
+
         for i in range(1, n + 1):
-            j = 1
-            while j * j <= i:
-                dp[i] = min(dp[i], dp[i - j * j] + 1)
-                j += 1
-        
+            for num in nums:
+                if i - num >= 0:
+                    dp[i] = min(dp[i], dp[i - num] + 1)
+
         return dp[n]
