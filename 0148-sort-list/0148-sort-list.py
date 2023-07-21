c31839513a822ddd9372d7c0853abc7dafd4892c
Abhinay <abhinayreddy0112@gmail.com>
2023-07-21 12:59:42 +0530

    Time: 647 ms (47.96%), Space: 38.7 MB (89.93%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -1,20 +1,40 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
-        val=[]
-        cur=head
-        while cur:
-            val.append(cur.val)
-            cur=cur.next
-        val.sort()
-        cur=head
-        for v in val:
-            cur.val=v
-            cur=cur.next
-        return head
+        def sortl(root):
+            def findmid(node):
+                if not node or not node.next:
+                    return node
+                
+                fast , slow =node.next, node
+                while fast and fast.next:
+                    fast = fast.next.next
+                    slow = slow.next
+                return slow
+            
+            def merge(l1, l2):
+                dumm = tail = ListNode()
+                while l1 and l2:
+                    if l1.val < l2.val:
+                        tail.next = l1
+                        l1 = l1.next
+                    else:
+                        tail.next = l2
+                        l2 = l2.next
+                    tail = tail.next
+                tail.next = l1 or l2
+                return dumm.next
+
+            if not root or not root.next:
+                return root
+            
+            left = root
+            mid = findmid(root)
+            right = mid.next
+            mid.next = None
+
+            left = sortl(left)
+            right = sortl(right)
+            return merge(left, right)
+
+        return sortl(head)
+
