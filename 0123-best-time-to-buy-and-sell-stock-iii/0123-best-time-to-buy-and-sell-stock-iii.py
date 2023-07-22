27037d07c349d2c7963229ed02572162f0d92b50
Abhinay <abhinayreddy0112@gmail.com>
2023-07-22 10:51:06 +0530

    Time: 4263 ms (7.05%), Space: 547 MB (7.14%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,18 +1,15 @@
 class Solution:
-    def maxProfit(self, p: List[int]) -> int:
-        dp = {}
-
-        def rec(i, bol, cnt):
-            if cnt == 0:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp={}
+        n=len(prices)
+        def rec(i,bol,cnt):
+            if cnt==0 or i==n:
                 return 0
-            if i == len(p):
-                return 0
-            if (i, bol, cnt) not in dp:
-                # Added the `not in` check here
-                if bol:
-                    dp[(i, bol, cnt)] = max(-p[i] + rec(i + 1, not bol, cnt ), rec(i + 1, bol, cnt))
-                else:
-                    dp[(i, bol, cnt)] = max(p[i] + rec(i + 1, not bol, cnt-1), rec(i + 1, bol, cnt))
-            return dp[(i, bol, cnt)]
-
-        return rec(0, True, 2)
+            if (i,bol,cnt) in dp:
+                return dp[(i,bol,cnt)]
+            if bol:
+                dp[(i,bol,cnt)]=max(rec(i+1,not bol,cnt-1)+prices[i],rec(i+1,bol,cnt))
+            else:
+                dp[(i,bol,cnt)]=max(rec(i+1,not bol,cnt)-prices[i],rec(i+1,bol,cnt))
+            return dp[(i,bol,cnt)]
+        return rec(0,False,2)
