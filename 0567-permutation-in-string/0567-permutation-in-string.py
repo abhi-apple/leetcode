8cef23242e704207da43038aa36fed5414033f7a
Abhinay <abhinayreddy0112@gmail.com>
2023-09-08 11:37:31 +0530

    Time: 85 ms (50.48%), Space: 16.4 MB (54.85%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,16 +1,40 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         l1=list(s1)
+        
         l1.sort()
+        
         l2=list(s2)
+        
+        
+        
+        j=len(l1)-1
+        
+        dicl1={}
+        
+        for i in l1:
+            if i in dicl1:
+                dicl1[i]+=1
+            else:
+                dicl1[i]=1
+        
+        dictl2=Counter(l2[:len(l1)])
         i=0
         j=len(l1)-1
-        while j<len(s2):
-            com=l2[i:j+1]
-            com.sort()
-            if l1==com:
+        while j<len(l2):
+            if dictl2==dicl1:
                 return True
-            i+=1
-            j+=1
+            else:
+                dictl2[l2[i]]-=1
+                if dictl2[l2[i]]==0:
+                    del dictl2[l2[i]]
+                i+=1
+                j+=1
+                if j<len(l2):
+                    if l2[j] in dictl2:
+                        dictl2[l2[j]]+=1
+                    else:
+                        dictl2[l2[j]]=1
+                
         return False
         
