fab15d1d66ab467ba4ab000ebc0dc7da2df9fd91
Abhinay <abhinayreddy0112@gmail.com>
2024-01-19 07:06:50 +0530

    Time: 142 ms (43.27%), Space: 26.9 MB (8.66%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,23 +1,20 @@
-from typing import List
-
 class Solution:
-    def minFallingPathSum(self, mat: List[List[int]]) -> int:
-        dic = {}
-        ans = []
-
-        def rec(i, j):
-            if i == 0 and j>=0 and j<len(mat[0]):
-                
-                return mat[i][j]
-            if i < 0 or j < 0 or  j>=len(mat[0]):
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        dp={}
+        n=len(matrix)
+        mini=float('inf')
+        def rec(i,j):
+            if (i,j) in dp:
+                return dp[(i,j)]
+            if i==0 and 0<=j<n:
+                return matrix[i][j]
+            if 0>i or i>n-1 or 0>j or j>n-1:
                 return float('inf')
-            if (i, j) in dic:
-                return dic[(i, j)]
-         
+            
+            dp[(i,j)]=min(rec(i-1,j),rec(i-1,j-1),rec(i-1,j+1))+matrix[i][j]
+            return dp[(i,j)]
+        for i in range(n):
+            mini=min(mini,rec(n-1,i))
+        return mini
         
-            dic[(i, j)] = mat[i][j] + min(rec(i - 1, j - 1), rec(i - 1, j), rec(i - 1, j + 1))
-            return dic[(i, j)]
-
-        for i in range(len(mat[0])):
-            ans.append(rec(len(mat) - 1, i))
-        return min(ans)
+                
